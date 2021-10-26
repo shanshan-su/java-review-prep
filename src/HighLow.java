0f64c8512b5c0c2766ec5e4d1b46b6e968bf1746
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-10-26 09:06:27 -0500

    feat: wrapped up bonus

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,3 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class HighLow {
     // 5. Game Development 101
     //
@@ -16,4 +20,44 @@ public class HighLow {
     //Hints
     //
     //Use the random method of the java.lang.Math class to generate a random number.
+
+    // Bonus
+    //
+    //Keep track of how many guesses a user makes.
+    //Set an upper limit on the number of guesses.
+
+    public static void playGuessGame() {
+        Scanner scanner =  new Scanner(System.in);
+        int random = (int) (Math.random() * 100 + 1);
+
+        System.out.println("Howdy there! Welcome to the guess game.");
+        System.out.println("I have a number between 1 and 100:)");
+        int userNum;
+        int guessCount = 0;
+        int maxGuess = 7;
+
+        do {
+            System.out.println("Please make a guess. Enter a number.");
+            userNum = scanner.nextInt();
+            guessCount += 1;
+
+            if (userNum < random) {
+                System.out.println("HIGHER!");
+            } else if (userNum > random) {
+                System.out.println("LOWER!");
+            }
+        } while (userNum != random && guessCount <= maxGuess);
+
+        if (userNum == random && guessCount <= maxGuess) {
+            System.out.println("GOOD GUESS!");
+        } else {
+            System.out.println("I'm sorry you have guessed too many times!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        playGuessGame();
+    }
+
 }
